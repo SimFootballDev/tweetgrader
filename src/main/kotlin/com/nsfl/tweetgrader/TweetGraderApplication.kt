e890f708be0f812d521597d954008ea223097eba
sfatihterim <sfatihterim@yandex.com>
2019-04-28 23:47:20 -0700

    Use Jsoup to parse username twitter pairs

--- src/main/kotlin/com/nsfl/tweetgrader/TweetGraderApplication.kt
+++ src/main/kotlin/com/nsfl/tweetgrader/TweetGraderApplication.kt
@@ -1,17 +1,22 @@
 package com.nsfl.tweetgrader
 
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import twitter4j.Status
 import twitter4j.TwitterFactory
 import twitter4j.conf.ConfigurationBuilder
 import java.text.SimpleDateFormat
 import java.util.*
+import java.util.regex.Pattern
+import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
 
-private const val TWEETS_HTML = "<!doctypehtml><link href=https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css rel=stylesheet><link href=https://cdn.datatables.net/select/1.3.0/css/select.dataTables.min.css rel=stylesheet><link href=https://cdn.datatables.net/buttons/1.5.6/css/buttons.dataTables.min.css rel=stylesheet><script src=https://code.jquery.com/jquery-3.3.1.js></script><script src=https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js></script><script src=https://cdn.datatables.net/select/1.3.0/js/dataTables.select.min.js></script><script src=https://cdn.datatables.net/buttons/1.5.6/js/dataTables.buttons.min.js></script><script class=init>var dataSet = [%s]; \$(document).ready(function() { var table = \$('#table').DataTable({ paging: false, order: [ [0, \"asc\"] ], data: dataSet, columns: [ { title: 'Username' }, { title: 'Content' }, { title: 'Likes' }, { title: 'Retweets' }, { title: '' }], columnDefs: [ { orderable: false, className: 'select-checkbox', targets: 4 } ], select: { style: 'multi' }, dom: 'Bfrtip', buttons: [ { text: 'Generate Payouts', action: function () { var selected = table.rows( { selected: true } ); var usernameList = \"\"; for (var i = 0; i < selected.count(); i++) { usernameList += selected.data()[i][0] + ','; } window.location.assign('/payouts?usernameList=' + usernameList + '&dateRange=%s'); } } ] }) });</script><style>div{padding-left:5%%;padding-right:5%%;padding-top:.5%%;padding-bottom:.5%%}</style><div><table class=\"celled table ui\"id=table width=100%%></table></div>"
+private const val TWEETS_HTML = "<!doctypehtml><link href=https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css rel=stylesheet><link href=https://cdn.datatables.net/select/1.3.0/css/select.dataTables.min.css rel=stylesheet><link href=https://cdn.datatables.net/buttons/1.5.6/css/buttons.dataTables.min.css rel=stylesheet><script src=https://code.jquery.com/jquery-3.3.1.js></script><script src=https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js></script><script src=https://cdn.datatables.net/select/1.3.0/js/dataTables.select.min.js></script><script src=https://cdn.datatables.net/buttons/1.5.6/js/dataTables.buttons.min.js></script><script class=init>var dataSet = [%s]; \$(document).ready(function() { var table = \$('#table').DataTable({ paging: false, order: [ [0, \"asc\"] ], data: dataSet, columns: [ { title: 'Username' }, { title: 'Link' }, { title: 'Content' }, { title: 'Likes' }, { title: 'Retweets' }, { title: '' }], columnDefs: [ { orderable: false, className: 'select-checkbox', targets: 5 } ], select: { style: 'multi' }, dom: 'Bfrtip', buttons: [ { text: 'Generate Payouts', action: function () { var selected = table.rows( { selected: true } ); var usernameList = \"\"; for (var i = 0; i < selected.count(); i++) { usernameList += selected.data()[i][0] + ','; } window.location.assign('/payouts?usernameList=' + usernameList + '&dateRange=%s'); } } ] }) });</script><style>div{padding-left:5%%;padding-right:5%%;padding-top:.5%%;padding-bottom:.5%%}</style><div><table class=\"celled table ui\"id=table width=100%%></table></div>"
 
 @RestController
 @SpringBootApplication
@@ -50,19 +55,70 @@ class TweetGraderApplication {
         end.set(Calendar.SECOND, 59)
         end.add(Calendar.DAY_OF_YEAR, 6)
 
-        // fixme: jsoup stuff
-        val userMap = mapOf(
-                Pair("Eco", "fatih_terim_22"),
-                Pair("Test", "fatih_terim_22")
-        )
+        val userList = ArrayList<Pair<String, String>>()
+        val documentList = ArrayList<Document>()
+
+        val firstDocument = Jsoup.connect(postUrl).get()
+        val firstDocumentBody = firstDocument.body().toString()
+        documentList.add(firstDocument)
+
+        val pageCount = try {
+            val startIndex = firstDocumentBody.indexOf("Pages:</a>")
+            val endIndex = firstDocumentBody.indexOf(")", startIndex)
+            firstDocumentBody.substring(startIndex, endIndex)
+                    .replace(Pattern.compile("[^0-9.]").toRegex(), "")
+                    .toInt()
+        } catch (exception: Exception) {
+            1
+        }
+
+        for (i in 1..(pageCount - 1)) {
+            documentList.add(
+                    Jsoup.connect(
+                            "$postUrl&st=${i * 14}"
+                    ).get()
+            )
+        }
+
+        documentList.forEach {
+            it.body().getElementsByClass("post-normal").forEach { element ->
+                val elementString = element.toString()
+                if (elementString.contains("twitter.com/")) {
+                    var completed = false
+                    var index = elementString.indexOf("twitter.com/") + 12
+                    var twitter = ""
+                    while (!completed) {
+                        val char = elementString[index]
+                        if (char != '"' && char != '<' && char != '/' && char != '?') {
+                            twitter += char
+                        } else {
+                            completed = true
+                        }
+                        index++
+                    }
+                    userList.add(Pair(element.getElementsByClass("normalname").text(), twitter))
+                }
+            }
+        }
 
         return TWEETS_HTML.format(
-                userMap.entries.joinToString(",") { entry ->
-                    twitter.timelines().getUserTimeline(entry.value).filter {
-                        it.createdAt.time > start.timeInMillis && it.createdAt.time < end.timeInMillis
-                    }.joinToString(",") { tweet ->
-                        "['${entry.key}', '${tweet.text.replace("'", "’")}', " +
-                                "'${tweet.favoriteCount}', '${tweet.retweetCount}', '']"
+                userList.joinToString(",") { pair ->
+                    val tweetList = try {
+                        twitter.timelines().getUserTimeline(pair.second).map { it }
+                    } catch (exception: Exception) {
+                        arrayListOf<Status>()
+                    }
+                    if (tweetList.isEmpty()) {
+                        "['${pair.first}', 'Link', 'Could not parse this user’s tweets.', '0', '0', '']"
+                    } else {
+                        tweetList.filter {
+                            it.createdAt.time > start.timeInMillis && it.createdAt.time < end.timeInMillis
+                        }.joinToString(",") { tweet ->
+                            "['${pair.first}', " +
+                                    "'<a href=\"https://www.twitter.com/${pair.second}/status/${tweet.id}\">Link</a>', " +
+                                    "'${tweet.text.replace("'", "’").replace("\n", " ")}', " +
+                                    "'${tweet.favoriteCount}', '${tweet.retweetCount}', '']"
+                        }
                     }
                 },
                 outputDateFormat.format(start.timeInMillis) +
